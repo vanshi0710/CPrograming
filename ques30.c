1840f382e41d84b9024e32816222a9dce7b29bdf
Vanshi0710 <Vanshikachawla0710@gmail.com>
2022-11-02 00:15:02 +0530

    Question 30

---
+++ ques30.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+float bill(int units){
+    float unit = (float)units;
+     float bill;
+    if(unit <= 50){
+        bill =  unit* 0.5;
+
+    }
+    else if(unit <=150){
+        bill = 25 + (unit-50)*0.75;
+
+    }
+    else if(unit <=250){
+        bill = 100 + (unit-150)*1.2;
+        
+    }
+    else{
+        bill  = 220 + (unit-250)*1.5;
+    }
+    bill = bill + bill*0.2;
+    return bill;
+
+}
+
+
+void main(){
+    int units;
+    float b;
+    printf("Enter number of units used : ");
+    scanf("%d",&units);
+    b = bill(units);
+    printf("Your bill is  %f",b);
+}
