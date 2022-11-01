31a3f2f4344d861e1de85def62bc307b659a8d80
Vanshi0710 <Vanshikachawla0710@gmail.com>
2022-11-01 23:46:39 +0530

    Ques19 to ques25

--- ques19.c
+++ ques19.c
@@ -23,7 +23,7 @@ char grade(float percentage){
 
 float percentage(int m[]){
     int sum = m[1] + m[2] + m[3] + m[4] + m[0];
-    float per = sum / 5;
+    float per = (float)sum / 5;
     return per;
 }
 
