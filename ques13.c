442f423c4caada8753b658c25a7d0da9f1e8e407
Vanshi0710 <Vanshikachawla0710@gmail.com>
2022-11-02 18:46:11 +0530

    Question13

--- ques13.c
+++ ques13.c
@@ -10,5 +10,16 @@ int noOfNotes(int amount , int note){
 
 
 void main(){
-    printf("%d", noOfNotes(10060,500));
+    int notes[]={10,20,50,100,200,500,2000};
+    int amount=0;
+    int n;
+    printf("Enter amount: ");
+    fflush(stdin);
+    scanf("%d",&amount);
+    printf("%d",amount);
+    for(int i = 0 ; i<7;i++){
+        n =noOfNotes(amount,notes[i]);
+        printf("Number of %d notes required are %d\n",notes[i],n);
+    }
+
 }
