197c27583ffe727d821ecea6ea30def06003c909
Vanshi0710 <Vanshikachawla0710@gmail.com>
2022-11-02 18:32:02 +0530

    Question10

--- ques10.c
+++ ques10.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
 
-#include <ctype.h>
-
-void charCase(char a){
-    if(isalpha(a)==1){
-        printf("UpperCase\n");
-    }else if(isalpha(a)==2){
-        printf("lowerCAse\n");
+int alpha(char a){
+    if(a>64 && a< 91){
+        return 1;
     }
-    else{
-        printf("not a alphabet\n");
+    else if( a >96 && 123){
+        return 2;
     }
+    return 0;
 }
 
 void main(){
-    charCase('=');
-    charCase('P');
-    charCase('u');
-    
+    char a;
+    printf("Enter a char value: ");
+    fflush(stdin);
+    scanf("%c",&a);
+    if(alpha(a)==1){
+        printf("UpperCase\n");
+    }else if(alpha(a)==2){
+        printf("lowerCAse\n");
+    }
+    else{
+        printf("not an alphabet\n");
+    }
 }
