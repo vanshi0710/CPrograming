6a82c0ad9221b130002224b3c4edec4fc4e9afc4
Vanshi0710 <Vanshikachawla0710@gmail.com>
2022-11-08 23:37:24 +0530

    Question 8

--- arrayQuestion8.c
+++ arrayQuestion8.c
@@ -1,7 +1,46 @@
 //  Write a program in C to sort elements of the array in descending order.
-
 #include<stdio.h>
 #include<stdlib.h>
+
+void swap(int arr[],int a,int b){
+    int c = arr[a];
+    arr[a]=arr[b] ;
+    arr[b] = c;
+}
+
+
+
+int partition(int arr[],int low,int high){
+    int pivot = arr[low];
+    int start = low;
+    int end = high;
+   while(start<end){
+    
+        while(arr[start]>=pivot){
+            
+            start++;
+        }
+        while(arr[end]<pivot){
+            
+            end--;
+        }  
+        if(start<end){
+            swap(arr,start,end);
+        }
+    }
+    swap(arr,low,end);
+    return end;
+   
+}
+void quicksort(int arr[],int low,int high){
+    int loc;
+    if(low<high){
+        loc = partition(arr,low,high);
+        quicksort(arr,low,loc);
+        quicksort(arr,loc+1,high);
+    }
+    
+}
 
 int* array(int n){
     int *arr = (int*)(malloc(sizeof(int) * n));
@@ -12,35 +51,19 @@ int* array(int n){
     return arr;
 }
 
-int* sort(int arr[],int size){
-    int temp;
-    for(int i = 0 ; i<size-1;i++){
-        for(int j = 0;j<size-i-1;j++){
-            if(arr[j]<arr[j+1]){
-                temp = arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-    return arr;
-}
-void printArray(int arr,int size){
-    for(int i=0;i<size;i++)  
-    {  
-        printf("%d ", arr[i]);  
-    } 
-}
-
 int main()  
 {  
-  int *arr,*sortedArray;  
+  int *arr,*sortedarray;  
   int size;
   printf("Input the number of elements to store in the array: ");
   scanf("%d",&size);
   arr=array(size);  
-  sortedArray=sort(arr,size);
+    
+  quicksort(arr,0,size);
   printf("\nElements of array in sorted ascending order: ");  
-  printArray(sortedArr,size); 
+  for(int i=0;i<size;i++)  
+    {  
+        printf("%d ", arr[i]);  
+    }  
     return 0;  
 }  
