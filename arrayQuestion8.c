579ebd7e94e91206cdb21a96447c7d842d1bcd6b
Vanshi0710 <Vanshikachawla0710@gmail.com>
2022-11-08 23:20:13 +0530

    Question 8

--- arrayQuestion8.c
+++ arrayQuestion8.c
@@ -12,8 +12,6 @@ int* array(int n){
     return arr;
 }
 
-
-
 int* sort(int arr[],int size){
     int temp;
     for(int i = 0 ; i<size-1;i++){
@@ -27,19 +25,22 @@ int* sort(int arr[],int size){
     }
     return arr;
 }
+void printArray(int arr,int size){
+    for(int i=0;i<size;i++)  
+    {  
+        printf("%d ", arr[i]);  
+    } 
+}
 
 int main()  
 {  
-  int *arr,*sortedarray;  
+  int *arr,*sortedArray;  
   int size;
   printf("Input the number of elements to store in the array: ");
   scanf("%d",&size);
   arr=array(size);  
-   sortedarray=sort(arr,size);
+  sortedArray=sort(arr,size);
   printf("\nElements of array in sorted ascending order: ");  
-  for(int i=0;i<size;i++)  
-    {  
-        printf("%d ", sortedarray[i]);  
-    }  
+  printArray(sortedArr,size); 
     return 0;  
 }  
