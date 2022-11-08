bcf7ff4d1d2e7ac39836ff4ff8ff8bcb34efd433
Vanshi0710 <Vanshikachawla0710@gmail.com>
2022-11-08 23:30:26 +0530

    Question 5

--- arrayQuestion5.c
+++ arrayQuestion5.c
@@ -1,27 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void swap(int arr[],int a,int b){
+    int c = arr[a];
+    arr[a]=arr[b] ;
+    arr[b] = c;
+}
+
+
+
+int partition(int arr[],int low,int high){
+    int pivot = arr[low];
+    int start = low;
+    int end = high;
+   while(start<end){
+    
+        while(arr[start]<=pivot){
+            start++;
+        }
+        while(arr[end]>pivot){
+            end--;
+        }  
+        if(start<end){
+            swap(arr,start,end);
+        }
+    }
+    swap(arr,low,end);
+    return end;
+   
+}
+void quicksort(int arr[],int low,int high){
+    int loc;
+    if(low<high){
+        loc = partition(arr,low,high);
+        quicksort(arr,low,loc);
+        quicksort(arr,loc+1,high);
+    }
+    
+}
+
 int* array(int n){
     int *arr = (int*)(malloc(sizeof(int) * n));
     printf("Enter elements of array:\n");
     for(int i = 0;i<n ;i++){
         scanf("%d",&arr[i]);
-    }
-    return arr;
-}
-
-
-
-int* sort(int arr[],int size){
-    int temp;
-    for(int i = 0 ; i<size-1;i++){
-        for(int j = 0;j<size-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                temp = arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
     }
     return arr;
 }
@@ -33,11 +55,12 @@ int main()
   printf("Input the number of elements to store in the array: ");
   scanf("%d",&size);
   arr=array(size);  
-   sortedarray=sort(arr,size);
+    
+  quicksort(arr,0,size);
   printf("\nElements of array in sorted ascending order: ");  
   for(int i=0;i<size;i++)  
     {  
-        printf("%d ", sortedarray[i]);  
+        printf("%d ", arr[i]);  
     }  
     return 0;  
 }  
